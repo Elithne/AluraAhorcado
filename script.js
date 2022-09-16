c9e77055dacd6f504f5e367b8ef34a89f10d6602
Giannina <gftartaglia@gmail.com>
2022-09-16 12:35:21 -0300

    added more logic to the word discovery

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let palabraEscogida = "HTML";
-let palabrasSecretas = ["HTML", "Web","JavaScript","Alura"];
+let palabrasSecretas = ["HTML", "WEB","JAVASCRIPT","ALURA"];
 let cantidadGuiones = 0;
 let pantalla = document.querySelector("canvas");
 let fondo = pantalla.getContext("2d"); 
@@ -11,16 +11,24 @@ let posicionLineaActual = posicionLineaInicial;
 let baseLinea = 20;
 let alturaLinea = 5;
 let espacioEntreLineas = 50;
+let letrasIncorrectas = document.getElementById('letrasIncorrectas');
+
+function dibujarAhorcado(){
+    actualizarPantalla()
+    dibujarGuiones(contarGuiones(escogerPalabraSecreta()))
+}
 
 function escogerPalabraSecreta(){
     palabraEscogida = palabrasSecretas[Math.floor(Math.random() * palabrasSecretas.length)]
-    console.log(palabraEscogida);
-    actualizarPantalla()
-    dibujarGuiones(contarGuiones(palabraEscogida));
-    
+    console.log(palabraEscogida); 
+}
+
+function getPalabraEscogida(){
+    return palabraEscogida;
 }
 
 function contarGuiones(palabraEscogida){
+    palabraEscogida = getPalabraEscogida();
     let cantidadGuiones = palabraEscogida.length;
     return cantidadGuiones;
 }
@@ -38,14 +46,42 @@ function actualizarPantalla(){
     fondo.fillRect(0, 0, 600, 400);
 }
 
-function verificarTeclaPresionada(){
-
+function verificarTeclaPresionada(teclaPresionada){
+    let tecla = teclaPresionada;    
+        if(tecla.toLowerCase() !== tecla.toUpperCase()){
+            //console.log("La tecla es una letra válida");
+            verificarLetraEnPalabra(teclaPresionada);
+            return tecla;
+        } else {
+            console.log("la tecla no es una letra válida")
+        }      
 }
 
-function dibujarLetraCorrecta(){
+function verificarLetraEnPalabra(teclaPresionada){
+    let tecla = teclaPresionada.toUpperCase();
+    let palabra = getPalabraEscogida();
+    for(let i = 0; i < palabra.length; i++){
+        if(tecla == palabra[i]){
+            dibujarLetraCorrecta(palabra[i]);
+        } else{
+            dibujarLetraIncorrecta();
 
+        }
+    }   
+}
+function dibujarLetraCorrecta(letraCorrecta){
+    let letra = letraCorrecta;
+    console.log(letra);
 }
 
 function dibujarLetraIncorrecta(){
+    console.log("_");
 
 }
+
+//El evento 'keydown' dispara cuando una letra se presiona, el parámetro e contiene información
+//sobre la acción ocasionada 
+document.addEventListener("keydown", e=>{
+    verificarTeclaPresionada(e.key)
+});
+dibujarAhorcado()
